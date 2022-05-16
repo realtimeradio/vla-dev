fd930c1f5242c51995ec3d057a0c6cfb738636d2
radonnachie <code@radonn.co.za>
2022-05-16 14:46:50 +0000

    * cosmic_f_remote CosmicFengine arguments

--- sw/control_remote_sw/src/cosmic_fengine.py
+++ sw/control_remote_sw/src/cosmic_fengine.py
@@ -1,7 +1,7 @@
 from remoteobjects.client import defineRemoteClass
 
 class CosmicFengine():
-    def __new__(cls, host, remoteobject_uri, neths=1, logger=None):
+    def __new__(cls, host, remoteobject_uri, pipeline_id=0, attribute_depth_allowance=1):
         """
         This is prioritised constructor method. If remoteobject_uri is specified, a CosmicFengineRemote class instance
         is returned instead of the expected `CosmicFengine` instance. In the latter case, the `__init__` method is
@@ -13,6 +13,6 @@ class CosmicFengine():
 						globals(),
 						delete_remote_on_del=False,
 						allowed_upload_extension_regex=r'\.fpg|\.yaml',
-						attribute_depth_allowance=1,
+						attribute_depth_allowance=attribute_depth_allowance
 				)
 				return CosmicFengineRemote(remote_object_id=f'{host}_{pipeline_id}')
