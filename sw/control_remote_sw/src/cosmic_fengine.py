777b7e1f8f6e05c1fe6473c503af90fadd68a060
radonnachie <code@radonn.co.za>
2022-05-16 15:43:19 +0000

    ^ remote cosmic_fengine whitespaces

--- sw/control_remote_sw/src/cosmic_fengine.py
+++ sw/control_remote_sw/src/cosmic_fengine.py
@@ -7,12 +7,13 @@ class CosmicFengine():
         is returned instead of the expected `CosmicFengine` instance. In the latter case, the `__init__` method is
         invoked with all the same arguments.
         """
-				defineRemoteClass(
-						'CosmicFengine',
-						remoteobject_uri,
-						globals(),
-						delete_remote_on_del=False,
-						allowed_upload_extension_regex=r'\.fpg|\.yaml',
-						attribute_depth_allowance=attribute_depth_allowance
-				)
-				return CosmicFengineRemote(remote_object_id=f'{host}_{pipeline_id}')
+        definition_dict={}
+        defineRemoteClass(
+                'CosmicFengine',
+                remoteobject_uri,
+                definition_dict,
+                delete_remote_on_del=False,
+                allowed_upload_extension_regex=r'\.fpg|\.yaml',
+                attribute_depth_allowance=attribute_depth_allowance
+        )
+        return definition_dict['CosmicFengineRemote'](remote_object_id=f'{host}_{pipeline_id}')
