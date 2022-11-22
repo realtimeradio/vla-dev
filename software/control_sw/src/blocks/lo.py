da12ba0c7c6f53ae191f5a6e65bbcca4328d2312
Jack <jack@realtimeradio.co.uk>
2022-11-22 14:51:24 +0000

    v13.7
    Increase LO precision to Fix45_44
    Allow periodic loading.
    
    The intention here is that we can now match the VLA's fshift
    settings, which appear to be multiples of 100Hz, to a precision
    of better than 1 mHz.
    The new firmware allows the LO accumulators to be periodically
    reset -- resetting every 1s would accumulate less than ~0.03 degrees
    of phase error vs the VLA over the coarse of integration.

--- software/control_sw/src/blocks/lo.py
+++ software/control_sw/src/blocks/lo.py
@@ -23,22 +23,34 @@ class Lo(Block):
     :type n_par_samples: int
 
     :param samplehz: The ADC sample rate in Hz
-    :type samplehz: float
+    :type samplehz: int
 
     """
-    _BP = 29
-    _BW = 32
+    _BW = 45 # bitwidth of phase increment register
+    _BP = 44 # binary point position of phase increment register
     MAX_PHASE_STEP = (2**_BW)//2 -1 
-    MAX_PHASE_OFFSET = (2**_BW)//2 -1 
     MIN_PHASE_STEP = -(2**_BW)//2
-    MIN_PHASE_OFFSET = -(2**_BW)//2
 
-    def __init__(self, host, name, n_streams=4, n_par_samples=8, samplehz=2048e6, logger=None):
+    def __init__(self, host, name, n_streams=4, n_par_samples=8, samplehz=2048000000, logger=None):
         super(Lo, self).__init__(host, name, logger)
         self.timer = TimedPulse(host, name+"_timing", logger)
         self.n_streams = n_streams
         self.n_par_samples = n_par_samples
         self.samplehz = samplehz
+        self.phase_step_reg_lsb = f"""{stream}_phase_step_lsb"""
+        self.phase_step_reg_msb = f"""{stream}_phase_step_msb"""
+
+    def set_reload_repeat_period(self, period):
+        """
+        After an initial load via the internal timer, repeat resetting / loading
+        of the phase accumulators every ``period`` ADC samples.
+        Use period=0 to disable periodic reloads.
+
+        :param period: Phase accumulator reset period, in ADC samples.
+        :type period: int
+        """
+        assert period % self.n_par_samples == 0, "Period not a multiple of FPGA period"
+        self.write_uint("reset_repeat_rate", period // self.n_par_samples)
 
     def set_phase_step(self, stream, phase_step):
         """
@@ -63,10 +75,12 @@ class Lo(Block):
             raise RuntimeError(message)
         if stream > self.n_streams:
             self._error(f"""Tried to set phase step for stream {stream} > n_streams ({self.n_streams})""")
-        phase_step_reg = f"""{stream}_phase_step"""
-        phase_step *= 2**self._BP
+        phase_step *= 2**(self._BP)
         self._debug(f"""Setting lo phase step of stream {stream} to {phase_step}""")
-        self.write_int(phase_step_reg, int(phase_step))
+        if phase_step < 0:
+            phase_step += 2**self._BW
+        self.write_int(self.phase_step_reg_lsb, phase_step & (2**32 - 1))
+        self.write_int(self.phase_step_reg_msb, phase_step >> 32)
 
     def force_load(self):
         """
@@ -81,62 +95,12 @@ class Lo(Block):
         :param stream: ADC stream index to which the phase should be applied.
         :type stream: int
         """
-        phase_step_reg = f"""{stream}_phase_step"""
-        return self.read_int(phase_step_reg)
-
-    def set_phase_offset(self, stream, phase_offset):
-        """
-        Set the phase offset for a given stream.
-
-        :param stream: ADC stream index to which the phase should should be applied.
-        :type stream: int
-
-        :param phase_offset: LO phase offset to load
-        :type delay: float
-        
-        """
-        if phase_offset >= self.MAX_PHASE_OFFSET:
-            message = f"""User requested a phase offset of {phase_offset}, 
-            maximum allowed is {self.MAX_PHASE_OFFSET}"""
-            self._error(message)
-            raise RuntimeError(message)
-        if phase_offset < self.MIN_PHASE_OFFSET:
-            message = f"""User requested a phase offset of {phase_offset}, 
-            minimum allowed is {self.MIN_PHASE_OFFSET}"""
-            self._error(message)
-            raise RuntimeError(message)
-        if stream > self.n_streams:
-            self._error(f"""Tried to set phase offset for stream {stream} > n_streams ({self.n_streams})""")
-        phase_offset_reg = f"""{stream}_phase_offset"""
-        phase_offset *= 2**self._BP
-        self._debug(f"""Setting lo phase offset of stream {stream} to {phase_offset}""")
-        self.write_int(phase_offset_reg, int(phase_offset))
-
-    def get_phase_offset(self, stream):
-        """
-        Retrieve the programmed phase offset for a given stream.
-
-        :param stream: ADC stream index to which the phase should be applied.
-        :type stream: int
-        """
-        phase_offset_reg = f"""{stream}_phase_offset"""
-        return self.read_int(phase_offset_reg)
-
-    def set_phase(self, stream, phase_offset):
-        """
-        Here we account for the number of streams being processed in parallel by the LO block.
-        Hence, phase_step = n_par_samples * phase_offset
-        
-        :param stream: ADC stream index to which the phase should be applied.
-        :type stream: int
-
-        :param phase_offset: LO phase offset to load
-        :type phase_offset: float
-        """
-        phase_step = phase_offset * self.n_par_samples
-        assert phase_step < self.MAX_PHASE_STEP, f"""The supplied phase offset corresponds to a phase_step = {phase_step} > {self.MAX_PHASE_STEP}"""
-        self.set_phase_offset(stream, phase_offset)
-        self.set_phase_step(stream, phase_step)
+        lsb = self.read_uint(self.phase_step_reg_lsb)
+        msb = self.read_uint(self.phase_step_reg_msb)
+        v = (msb << 32) + lsb
+        if v > self.MAX_PHASE_STEP:
+            v -= 2**self._BW
+        return v
 
     def set_lo_frequency_shift(self, stream, frequency_shift):
         """
@@ -151,8 +115,8 @@ class Lo(Block):
         """
         assert frequency_shift <= self.get_max_shift(), f"""Specified frequency_shift {frequency_shift}Hz is larger than the maximum shift allowed {self.get_max_shift()}Hz."""
         assert frequency_shift >= self.get_min_shift(), f"""Specified frequency_shift {frequency_shift}Hz is smaller than the minimum shift allowed {self.get_min_shift()}Hz."""
-        phase_offset = (2**(self._BW-self._BP))*(frequency_shift/(self.samplehz))
-        self.set_phase(stream, phase_offset)
+        phase_step = (frequency_shift/self.samplehz) * (2**self._BP)
+        self.set_phase_step(stream, phase_step)
 
     def get_lo_frequency_shift(self, stream, return_in_hz = True):
         """
@@ -163,24 +127,38 @@ class Lo(Block):
         :param stream: ADC stream index to which the phase should be applied.
         :type stream: int
 
-        :param return_in_hz: return the frequency shift in Hz if True otherwise return integer offset per ADC clock with it's bitwidth.
+        :param return_in_hz: return the frequency shift in Hz if True. Else
+            return a tuple (phase_step, scale), indicating that the underlying
+            phase is being incremented by phase_step*2pi radians every 2**scale
+            ADC samples.
         :type return_in_hz: bool
         """
-        offset = self.get_phase_offset(stream)
-
-        return  offset * (self.samplehz)/(2**self._BW) if return_in_hz else (offset , self._BW)
+        step = self.get_phase_step(stream)
+        if return_in_hz:
+            return step * self.samplehz / 2**self._BP
+        else:
+            return (step, self._BP)
 
     def get_max_shift(self):
         """
-        Calculate the maximum allowed frequency shift.
+        Calculate the maximum allowed frequency shift, in Hz
         """
-        return self.MAX_PHASE_OFFSET * (self.samplehz)/(2**self._BW)
+        return self.MAX_PHASE_OFFSET * self.samplehz / 2**self._BP
 
     def get_min_shift(self):
         """
-        Calculate the minimum allowed frequency shift.
+        Calculate the minimum allowed frequency shift, in Hz
         """
-        return self.MIN_PHASE_OFFSET * (self.samplehz)/(2**self._BW)
+        return self.MIN_PHASE_OFFSET * self.samplehz / 2**self._BP
+
+    def get_shift_resolution(self):
+        """
+        Calculate the resolution of the internal LO.
+
+        :return: LO frequency resolution, in Hz
+        :rtype: float
+        """
+        return self.samplehz / 2**self._BP
 
     def get_status(self):
         """
@@ -190,8 +168,9 @@ class Lo(Block):
 
             - shifthz<``n``>: Currently loaded lo frequency shift for ADC input index ``n``.
               in units of ADC samples.
-            - max_shift: The maximum lo offshift supported by the firmware.
-            - min_shift: The minimum lo offshift supported by the firmware.
+            - max_shifthz: The maximum lo offshift supported by the firmware, in Hz
+            - min_shifthz: The minimum lo offshift supported by the firmware, in Hz
+            - shift_reshz: The resolution of the LO shift, in Hz, supported by the firmware
 
         :return: (status_dict, flags_dict) tuple. `status_dict` is a dictionary of
             status key-value pairs. flags_dict is
@@ -204,6 +183,7 @@ class Lo(Block):
             stats[f"shifthz{stream}"] = self.get_lo_frequency_shift(stream)
         stats['max_shifthz'] = self.get_max_shift()
         stats['min_shifthz'] = self.get_min_shift()
+        stats['shift_reshz'] = self.get_shift_resolution()
         return stats,flags
 
     def initialize(self, read_only=False):
@@ -219,4 +199,5 @@ class Lo(Block):
         if not read_only:
             for i in range(self.n_streams):
                 self.set_lo_frequency_shift(i, 0.0)
+            self.set_reload_repeat_period(0) # Disable periodic loading
             self.force_load()
