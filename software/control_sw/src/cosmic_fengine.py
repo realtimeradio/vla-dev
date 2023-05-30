2ad49b651d1aff2a27383f0ece9c4efb10008de1
talonmyburgh <talon.myburgh@mydon.co.za>
2023-05-30 15:39:20 +0000

    *^ assert to less restricted warning log

--- software/control_sw/src/cosmic_fengine.py
+++ software/control_sw/src/cosmic_fengine.py
@@ -914,8 +914,8 @@ class CosmicFengine():
             self.logger.info(f"F-Shift load time set to {time.ctime(lo_load_time)}")
             time.sleep(lo_load_time - (time.time()))
             fshift_time_to_load = self.lo.get_time_to_load()/FPGA_CLOCK_RATE_HZ
-            assert np.isclose(fshift_time_to_load, 0.0, atol=1e-2), \
-                    f"After sleeping, time to load from the F-Engine {fshift_time_to_load}s is not near zero."
+            if not np.isclose(fshift_time_to_load, 0.0, atol=1e-1):
+                    self.logger.warn(f"After sleeping, time to load from the F-Engine {fshift_time_to_load}s is not near zero.")
         else:
             raise RuntimeError("Cannot set F-shift load time for time in the past.")
 
